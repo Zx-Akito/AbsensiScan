e86c70284c9e51c4a8b9d7183cc6c8657764afb9
Zx-Akito <zxakito89@gmail.com>
2022-04-11 13:43:02 +0700

    add timezone

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-    error_reporting(0);
+    date_default_timezone_set("Asia/Bangkok");
+    // error_reporting(0);
     session_start();
+    
     
     include ("app/db.php"); // Koneksi Database
     include ("app/function.php"); // Fungsi
